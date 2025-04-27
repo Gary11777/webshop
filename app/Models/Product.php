881f7d21367382402888cb62c7b01e51479d3363
Gary11777 <iharushakou@gmail.com>
2025-04-27 12:04:00 +0300

    E5 Designing Our Storefront

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
 {
@@ -16,6 +17,10 @@ class Product extends Model
         return $this->hasMany(ProductVariant::class);
     }
 
+    public function image(): HasOne
+    {
+        return $this->hasOne(Image::class)->ofMany('featured', 'max');
+    }
     public function images(): HasMany
     {
         return $this->hasMany(Image::class);
