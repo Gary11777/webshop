d95d67792df2847048634b684c917f42cda9cdf0
Gary11777 <iharushakou@gmail.com>
2025-05-18 11:03:02 +0300

    E14 Create the cart page

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', \App\Livewire\StoreFront::class)->name('home');
 Route::get('/product/{productId}', \App\Livewire\Product::class)
     ->name('product');
+Route::get('/cart', \App\Livewire\Cart::class)
+    ->name('cart');
 
 /*Route::get('/', function () {
     return view('welcome');
