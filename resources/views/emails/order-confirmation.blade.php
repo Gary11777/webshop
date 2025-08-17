ec9092c0c8705f592bf377de8df47414c0c6b8f8
Gary11777 <iharushakou@gmail.com>
2025-08-17 09:38:20 +0300

    28_deliver

--- resources/views/emails/order-confirmation.blade.php
+++ resources/views/emails/order-confirmation.blade.php
@@ -1,1 +1,17 @@
-{{ $order->id }}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Order Confirmation #{{ $order->id }}</title>
+</head>
+<body>
+   <p>
+   Hey {{ $order->user->name ?? 'Customer' }},
+   </p>
+   <p>
+   Your order #{{ $order->id }} has been received and is being processed.
+   </p>
+</body>
+</html>
