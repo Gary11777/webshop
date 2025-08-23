9d4fd222990b89d919c8a1d9662c50aec3f2f346
Gary11777 <iharushakou@gmail.com>
2025-08-23 12:27:03 +0300

    E30 Add a Button to View the Order

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ Route::get('/product/{productId}', \App\Livewire\Product::class)
     ->name('product');
 Route::get('/cart', \App\Livewire\Cart::class)
     ->name('cart');
-Route::get('/checkout-status', \App\Livewire\CheckoutStatus::class)->name('webshop.checkout-status');
 Route::get('/preview', function () {
     $order = Order::first();
     return new OrderConfirmation($order);
@@ -32,6 +31,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+
+    Route::get('/checkout-status', \App\Livewire\CheckoutStatus::class)->name('checkout-status');
+    Route::get('/order/{orderId}', \App\Livewire\ViewOrder::class)->name('view-order');
 });
 
 require __DIR__.'/auth.php';
