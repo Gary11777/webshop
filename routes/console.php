4b18d51b3d49c8430bd94f5801a9d2018e0034cb
Gary11777 <iharushakou@gmail.com>
2025-09-04 23:37:28 +0300

    E32_1 Send an Abandoned Cart Email to the Costomer

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
+use App\Console\Commands\AbandonedCart;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule commands
+app(Schedule::class)->command(AbandonedCart::class)->dailyAt('13:00');
